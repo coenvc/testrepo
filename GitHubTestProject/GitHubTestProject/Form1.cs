f5497d42824e72c31538a6d21685ea50c10d2a22
coenvc <coenvc@gmail.com>
2016-03-28 23:33:10 +0200

    Add functionality where the users sees a messagebox if he clicks 10 times

--- GitHubTestProject/GitHubTestProject/Form1.cs
+++ GitHubTestProject/GitHubTestProject/Form1.cs
@@ -12,9 +12,19 @@ namespace GitHubTestProject
 {
     public partial class Form1 : Form
     {
+        int count = 0; 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++; 
+            if(count >= 10)
+            {
+                MessageBox.Show("Why are yo still clicking bruh?");
+            }
         }
     }
 }
